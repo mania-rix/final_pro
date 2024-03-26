d0487d53bb3eda93739530adf1db57461918fb06
mania-rix <125951876+mania-rix@users.noreply.github.com>
2024-03-26 14:56:45 -0400

    tokenizing_v3 by Amir

--- tokenizing.c
+++ tokenizing.c
@@ -1,25 +1,27 @@
 // TOKENIZING MODULE SOURCE
 #define _CRT_SECURE_NO_WARNINGS
-#define BUFFER_SIZE 80
-#include"tokenizing.h"
-// V2
+#define BUFFER_SIZE 300
+#include "tokenizing.h"
+
+// V3
 void tokenizing(void) {
-    printf("**** Start of Tokenizing Phrases Demo ****\n");
-    char phrases[BUFFER_SIZE];
-    char* nextPhrase = NULL;
-    int phrasesCounter;
+    printf("**** Start of Tokenizing Sentences Demo ****\n");
+    char sentences[BUFFER_SIZE];
+    char* nextSentence = NULL;
+    int sentencesCounter;
     do {
-        printf("Type a few phrases separated by comma (q - to quit):\n");
-        fgets(phrases, BUFFER_SIZE, stdin);
-        phrases[strlen(phrases) - 1] = '\0';
-        if ((strcmp(phrases, "q") != 0)) {
-            nextPhrase = strtok(phrases, ",");
-            phrasesCounter = 1;
-            while (nextPhrase) {
-                printf("Phrase #%d is '%s'\n", phrasesCounter++, nextPhrase);
-                nextPhrase = strtok(NULL, ",");
+        printf("Type a few sentences separated by dot(q - to quit):\n");
+        fgets(sentences, BUFFER_SIZE, stdin);
+        sentences[strlen(sentences) - 1] = '\0';
+        if ((strcmp(sentences, "q") != 0)) {
+            nextSentence = strtok(sentences, ".");
+            sentencesCounter = 1;
+            while (nextSentence) {
+                printf("Sentence #%d is '%s'\n", sentencesCounter++, nextSentence);
+                nextSentence = strtok(NULL, ".");
             }
         }
-    } while (strcmp(phrases, "q") != 0);
-    printf("**** End of Tokenizing Phrases Demo ****\n");
-}
+    } while (strcmp(sentences, "q") != 0);
+    printf("**** End of Tokenizing Sentences Demo ****\n");
+
+}   
