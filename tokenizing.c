57e80fb5cb683bbbf8c24df785f34c01eaf3fb00
dilroop0 <165073419+dilroop0@users.noreply.github.com>
2024-03-26 14:56:26 -0400

    tokenizing_v2 by Dilroop

---
+++ tokenizing.c
@@ -0,0 +1,25 @@
+// TOKENIZING MODULE SOURCE
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
+#include"tokenizing.h"
+// V2
+void tokenizing(void) {
+    printf("**** Start of Tokenizing Phrases Demo ****\n");
+    char phrases[BUFFER_SIZE];
+    char* nextPhrase = NULL;
+    int phrasesCounter;
+    do {
+        printf("Type a few phrases separated by comma (q - to quit):\n");
+        fgets(phrases, BUFFER_SIZE, stdin);
+        phrases[strlen(phrases) - 1] = '\0';
+        if ((strcmp(phrases, "q") != 0)) {
+            nextPhrase = strtok(phrases, ",");
+            phrasesCounter = 1;
+            while (nextPhrase) {
+                printf("Phrase #%d is '%s'\n", phrasesCounter++, nextPhrase);
+                nextPhrase = strtok(NULL, ",");
+            }
+        }
+    } while (strcmp(phrases, "q") != 0);
+    printf("**** End of Tokenizing Phrases Demo ****\n");
+}
