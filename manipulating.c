122e818ec241f61b2dc66648c7787f1e2cefb606
mania-rix <125951876+mania-rix@users.noreply.github.com>
2024-03-26 14:55:28 -0400

    manipulating_v3 by Amir

--- manipulating.c
+++ manipulating.c
@@ -1,29 +1,28 @@
 // MANIPULATING SOURCE
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 80
-#include"manipulating.h"
-// V2
+#include "manipulating.h"
+
+// V3
 void manipulating(void) {
-    printf("**** Start of Comparing Strings Demo ****\n");
-    char compare1[BUFFER_SIZE];
-    char compare2[BUFFER_SIZE];
-    int result;
+    printf("**** Start of Searching Strings Demo ****\n");
+    char haystack[BUFFER_SIZE];
+    char needle[BUFFER_SIZE];
+    char* occurrence = NULL;
     do {
-        printf("Type the 1st string to compare (q - to quit):\n");
-        fgets(compare1, BUFFER_SIZE, stdin);
-        compare1[strlen(compare1) - 1] = '\0';
-        if (strcmp(compare1, "q") != 0) {
-            printf("Type the 2nd string to compare:\n");
-            fgets(compare2, BUFFER_SIZE, stdin);
-            compare2[strlen(compare2) - 1] = '\0';
-            result = strcmp(compare1, compare2);
-            if (result < 0)
-                printf("'%s' string is less than '%s'\n", compare1, compare2);
-            else if (result == 0)
-                printf("'%s' string is equal to '%s'\n", compare1, compare2);
+        printf("Type the string (q - to quit):\n");
+        fgets(haystack, BUFFER_SIZE, stdin);
+        haystack[strlen(haystack) - 1] = '\0';
+        if (strcmp(haystack, "q") != 0) {
+            printf("Type the substring:\n");
+            fgets(needle, BUFFER_SIZE, stdin);
+            needle[strlen(needle) - 1] = '\0';
+            occurrence = strstr(haystack, needle);
+            if (occurrence)
+                printf("'%s' found at %d position\n", needle, (int)(occurrence - haystack));
             else
-                printf("'%s' string is greater than '%s'\n", compare1, compare2);
+                printf("Not found\n");
         }
-    } while (strcmp(compare1, "q") != 0);
-    printf("**** End of Comparing Strings Demo ****\n");
+    } while (strcmp(haystack, "q") != 0);
+    printf("**** End of Searching Strings Demo ****\n");
 }
