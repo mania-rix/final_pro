4925a9f36691215f0da09b392b7403e200863360
dilroop0 <165073419+dilroop0@users.noreply.github.com>
2024-03-26 14:47:01 -0400

    converting_v2 by Dilroop

--- converting.c
+++ converting.c
@@ -1,21 +1,20 @@
 // CONVERTING MODULE SOURCE
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 80
-#include "converting.h"
-
-// V3
+#include"converting.h"
+// V2
 void converting(void) {
-    printf("**** Start of Converting Strings to long Demo ****\n");
-    char    longString[BUFFER_SIZE];
-    int     longNumber;
+    printf("**** Start of Converting Strings to double Demo ****\n");
+    char doubleString[BUFFER_SIZE];
+    double doubleNumber;
     do {
-        printf("Type an int numeric string (q - to quit):\n");
-        fgets(longString, BUFFER_SIZE, stdin);
-        longString[strlen(longString) - 1] = '\0';
-        if (strcmp(longString, "q") != 0) {
-            longNumber = atoi(longString);
-            printf("Converted number is %d\n", longNumber);
+        printf("Type the double numeric string (q - to quit):\n");
+        fgets(doubleString, BUFFER_SIZE, stdin);
+        doubleString[strlen(doubleString) - 1] = '\0';
+        if ((strcmp(doubleString, "q") != 0)) {
+            doubleNumber = atof(doubleString);
+            printf("Converted number is %f\n", doubleNumber);
         }
-    } while (strcmp(longString, "q") != 0);
-    printf("**** End of Converting Strings to long Demo ****\n");
+    } while (strcmp(doubleString, "q") != 0);
+    printf("**** End of Converting Strings to double Demo ****\n");
 }
